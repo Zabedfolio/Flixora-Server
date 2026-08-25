bbb410bc94a396456998ee9fe3e9fa63e000dc60
anuj paul <paulanuj27@gmail.com>
2026-08-25 18:28:19 +0600

    add: Create movieAssistant controller

--- src/controllers/ai.controller.ts
+++ src/controllers/ai.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { searchMovie } from "../services/tmdb.services";
-
+import { generateMovieResponse } from "../services/ai.services";
 
 export const searchMovieWithQueary = async (req:Request, res:Response): Promise<void> => {
     try{
@@ -17,3 +17,47 @@ export const searchMovieWithQueary = async (req:Request, res:Response): Promise<
         })
     }
 }
+
+
+
+export const movieAssistant = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+
+  try {
+
+    const { message } = req.body;
+
+    if (!message?.trim()) {
+
+      res.status(400).json({
+        success: false,
+        message: "Message is required",
+      });
+
+      return;
+    }
+
+    const response =
+      await generateMovieResponse(message);
+
+    res.status(200).json({
+      success: true,
+      data: response,
+    });
+
+  } catch (error) {
+
+    console.error(
+      "AI Controller Error:",
+      error
+    );
+
+    res.status(500).json({
+      success: false,
+      message:
+        "Something went wrong with Flixora AI.",
+    });
+  }
+};
